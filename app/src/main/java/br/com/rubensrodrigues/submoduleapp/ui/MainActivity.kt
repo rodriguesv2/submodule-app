86c9fb1b9032c420fb91f272a177e196f26ac4bc
Rubens <rubens.rodrigues@mobile2you.com.br>
2020-01-08 17:34:33 -0300

    Initial commit

---
+++ app/src/main/java/br/com/rubensrodrigues/submoduleapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.rubensrodrigues.submoduleapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rubensrodrigues.submoduleapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
