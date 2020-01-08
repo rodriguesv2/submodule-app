7267092a01ce6eecaeac1c6241a0080439142c5a
Rubens <rubens.rodrigues@mobile2you.com.br>
2020-01-08 18:46:51 -0300

    models as repo

--- app/src/main/java/br/com/rubensrodrigues/submoduleapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/submoduleapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.rubensrodrigues.submoduleapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.rubensrodrigues.models.Person
 import br.com.rubensrodrigues.submoduleapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val person = Person("Beltrano", 30)
     }
 }
